4540ad3f3aa6c91c4a9b535df7a29c73376d52ef
Davselli <75071169+Davselli@users.noreply.github.com>
2023-08-01 17:33:10 +0200

    parallax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1096,9 +1096,8 @@
 
 })(window.jQuery);
 
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Initialize AOS
-    AOS.init();
+window.addEventListener('scroll', function () {
+    let offset = window.pageYOffset;
+    let parallaxElement = document.querySelector('.parallax');
+    parallaxElement.style.backgroundPositionY = offset * 0.7 + 'px';
   });
-  
