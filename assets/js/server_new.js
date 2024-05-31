7db868aa807593c0773eca7d5b314d9bfe12a4ac
Davselli <139363921+davselliTigera@users.noreply.github.com>
2024-05-31 15:41:43 +0200

    updated js

--- assets/js/server_new.js
+++ assets/js/server_new.js
@@ -11,7 +11,7 @@ document.getElementById('uploadButton').addEventListener('click', async () => {
         const content = event.target.result.split(',')[1]; // Get the base64 content
         const filename = file.name;
 
-        const token = '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'; // Replace with your GitHub Personal Access Token
+        const token = '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'; // Replace with your GitHub Personal Access Token
         const repoOwner = 'Davselli'; // Replace with your GitHub username
         const repoName = 'pic_wedding'; // Replace with your repository name
         const branchName = 'main'; // Replace with the branch you want to upload to
